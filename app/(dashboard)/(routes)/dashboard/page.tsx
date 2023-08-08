d4996036a14b47e6a21f17b739b39d5cabcc0ae2
gilangarinata <gilangarinata@gmail.com>
2023-08-08 11:31:57 +0700

    routing dashboard and landing

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import {Button} from "@/components/ui/button";
+
+const DashboardPage = () => {
+    return (
+        <p>Dashboard Page (protected)</p>
+    )
+}
+
+export default DashboardPage;
---
+++ app/(landing)/page.tsx
@@ -0,0 +1,9 @@
+const LandingPage = () => {
+    return (
+        <>
+             Landing Pgae (Unprotected)
+        </>
+    )
+}
+
+export default LandingPage;
