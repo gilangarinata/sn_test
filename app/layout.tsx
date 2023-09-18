691b1be15d3d965989313680204cc6393e95ce9a
gilangarinata <gilangarinata@gmail.com>
2023-09-18 10:35:48 +0700

    fix landign asas font gg 2

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Poppins } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs'
 import "tw-elements-react/dist/css/tw-elements-react.min.css";
 import '@fontsource/poppins';
+import Head from "next/head";
 
 
 const poppins = Poppins({ subsets: ['latin'], weight: "400" })
@@ -22,6 +23,15 @@ export default function RootLayout({
 
       <ClerkProvider>
         <html lang="en">
+        <Head>
+            {/* link your fonts here */}
+            <link rel="preconnect" href="https://fonts.googleapis.com" />
+            <link rel="preconnect" href="https://fonts.gstatic.com" />
+            <link
+                href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800;900&display=swap"
+                rel="stylesheet"
+            />
+        </Head>
           <body className={poppins.className}>{children}</body>
         </html>
       </ClerkProvider>
