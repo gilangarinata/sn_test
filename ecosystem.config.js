6d61ffaa085a1971183983523b5d1375aec71a8a
gilangarinata <gilangarinata@gmail.com>
2023-10-04 23:28:52 +0700

    a

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,7 +8,7 @@ module.exports = {
       host : '193.203.163.79',
       ref  : 'origin/main',
       repo : 'https://github.com/gilangarinata/sn_test.git',
-      path : '/home/ubuntu',
+      path : '/home/ubuntu/v2',
       'pre-deploy-local': '',
       'post-deploy' : 'source ~/.nvm/nvm.sh && npm install && npm run build && pm2 reload ecosystem.config.js --env production',
       'pre-setup': '',
