55600a93b064528f256ccdd83077b4207e71785b
gilangarinata <gilangarinata@gmail.com>
2023-11-14 17:25:55 +0700

    as

--- app/(landing)/media/news/page.tsx
+++ app/(landing)/media/news/page.tsx
@@ -15,11 +15,11 @@ import {Category} from "@/components/admin/media/category/category-table";
 
 async function MediaPage() {
     const categories = await fetchCategories("news")
-
+    const news = await fetchAllNews(1, 200)
     return (
        <div className="h-full">
            <NewsBanner image="" title="" />
-           <NewsContent categories={categories?.categories as Category[]} />
+           <NewsContent newsA={news?.banners as News[]} categories={categories?.categories as Category[]} />
        </div>
     )
 }
