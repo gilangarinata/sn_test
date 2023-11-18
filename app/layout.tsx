87c281d933b01e6d264fdaba258ce974355224f1
gilangarinata <gilangarinata@gmail.com>
2023-11-18 20:34:54 +0700

    asas

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-      <ClerkProvider afterSignInUrl="https://sesna.id/admin-panel">
+      <ClerkProvider>
         <html lang="en">
             <Head>
                 <link
