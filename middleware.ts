844ea54382854cc68e09083f6128314a2c0635a3
gilangarinata <gilangarinata@gmail.com>
2023-11-18 15:56:07 +0700

    a

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,7 @@ import { authMiddleware } from "@clerk/nextjs";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/nextjs/middleware for more information about configuring your middleware
 export default authMiddleware({
-    debug: true,
+    afterAuth: (auth) => { console.log("afterAuth", auth); },
     publicRoutes: ["/","/media/(.*)","/media/(.*)", "/who-we-are","/our-business","/get-in-touch","/zero-capex","/career/(.*)","/career","/zero-capex-result"]
 });
 
