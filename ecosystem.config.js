5a5d12f1d0b480cbfb4ec864090808de5a9c4780
gilangarinata <gilangarinata@gmail.com>
2023-11-19 13:35:27 +0700

    as

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   apps : [{
-    script: 'npm start',
+    script: 'npm run dev',
   }],
   deploy : {
     production : {
@@ -10,7 +10,8 @@ module.exports = {
       repo : 'https://github.com/gilangarinata/sn_test.git',
       path : '/home/ubuntu',
       'pre-deploy-local': '',
-      'post-deploy' : 'source ~/.nvm/nvm.sh && npm install && npm run build && pm2 reload ecosystem.config.js --env production',
+      // 'post-deploy' : 'source ~/.nvm/nvm.sh && npm install && npm run build && pm2 reload ecosystem.config.js --env production',
+      'post-deploy' : 'source ~/.nvm/nvm.sh && npm install && npm run dev && pm2 reload ecosystem.config.js',
       'pre-setup': '',
       'ssh_options': 'ForwardAgent=yes'
     }
