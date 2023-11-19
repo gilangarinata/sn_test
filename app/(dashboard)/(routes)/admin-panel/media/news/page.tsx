7fcad98ccb298724518fc6ad6805b65f35ab7c73
gilangarinata <gilangarinata@gmail.com>
2023-11-19 11:36:36 +0700

    as

--- app/(dashboard)/(routes)/admin-panel/media/news/page.tsx
+++ app/(dashboard)/(routes)/admin-panel/media/news/page.tsx
@@ -1,9 +1,3 @@
-import {fetchBanners} from "@/lib/actions/admin/banner.action";
-import UniversalBanners from "@/components/admin/home/banners/universal-banners";
-import MainBanner from "@/components/admin/home/banners/main-banner";
-import ExperienceTable from "@/components/admin/home/experience/experience-table";
-import AchievementTable from "@/components/admin/home/achievement/achievement-table";
-import CategoryTable from "@/components/admin/media/category/category-table";
 import NewsTable from "@/components/admin/media/news/news-table";
 
 
