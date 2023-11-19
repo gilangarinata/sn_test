938345080f650d160a626ccaaa4e74e4051178c0
gilangarinata <gilangarinata@gmail.com>
2023-11-19 14:59:45 +0700

    as

--- next.config.js
+++ next.config.js
@@ -3,12 +3,16 @@ const nextConfig = {
     images: {
         domains: ['images.unsplash.com', 'utfs.io', 'sesna.id', 'img.youtube.com']
     },
+    experimental: {
+        serverActions: {
+            allowedOrigins: ['https://sesna.id', 'https://www.sesna.id', 'https://utfs.io', 'https://www.utfs.io','http://localhost:3010'],
+        },
+    },
     optimizeFonts: false,
     compress: false,
     compiler: {
         removeConsole: false,
     },
-
 }
 
 module.exports = nextConfig
