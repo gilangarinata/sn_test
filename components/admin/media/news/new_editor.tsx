e51a99f2ed4d0fee46547d5a2e04ef72d4795ad9
gilangarinata <gilangarinata@gmail.com>
2023-11-30 11:09:21 +0700

    asa

--- components/admin/media/news/new_editor.tsx
+++ components/admin/media/news/new_editor.tsx
@@ -6,13 +6,63 @@ import {
 } from "@blocknote/core";
 
 import {
-    BlockNoteView,
+    BlockNoteView, lightDefaultTheme, Theme,
     useBlockNote
 } from "@blocknote/react";
 
 import "@blocknote/core/style.css";
 import {useTheme} from "@emotion/react";
 import {cn} from "@/lib/utils";
+import "@/app/custom.css";
+
+
+const lightRedTheme = {
+    colors: {
+        editor: {
+            text: "#222222",
+            background: "#ffffff",
+        },
+        menu: {
+            text: "#ffffff",
+            background: "#9b0000",
+        },
+        tooltip: {
+            text: "#ffffff",
+            background: "#b00000",
+        },
+        hovered: {
+            text: "#ffffff",
+            background: "#b00000",
+        },
+        selected: {
+            text: "#ffffff",
+            background: "#c50000",
+        },
+        disabled: {
+            text: "#9b0000",
+            background: "#7d0000",
+        },
+        shadow: "#640000",
+        border: "#870000",
+        sideMenu: "#bababa",
+        highlightColors: lightDefaultTheme.colors.highlightColors,
+    },
+    borderRadius: 4,
+    fontFamily: "Helvetica Neue, sans-serif",
+} satisfies Theme;
+
+const theme = {
+    ...lightRedTheme,
+    componentStyles: (theme) => ({
+        // Adds basic styling to the editor.
+        Editor: {
+            // backgroundColor: theme.colors.editor.background,
+            // borderRadius: theme.borderRadius,
+            // border: `1px solid ${theme.colors.border}`,
+            // boxShadow: `0 4px 12px ${theme.colors.shadow}`,
+        },
+    }),
+} satisfies Theme;
 
 interface Props {
     onChange : (value : string) => void;
@@ -40,12 +90,18 @@ export const NewEditor = ({
         initialContent: initialContent && isJsonString(initialContent) ? JSON.parse(initialContent) as PartialBlock[] : undefined,
         onEditorContentChange: (content) => {
             onChange(JSON.stringify(content.topLevelBlocks, null,2));
-        }
+        },
+        domAttributes: {
+            // Adds a class to all `blockContainer` elements.
+            blockContainer: {
+                class: "block-container",
+            },
+        },
     });
 
     return (
         <div>
-            <BlockNoteView className={cn("text-justify", editable ? "mx-0" : "mx-[-50px]")} editor={editor} theme="light" />
+            <BlockNoteView className={cn("text-justify", editable ? "mx-0" : "mx-[-50px]")} editor={editor} theme="light"/>
         </div>
     )
 }
