0fe8f29c389b99c15a8f9de4505d61b7919aae9e
gilangarinata <gilangarinata@gmail.com>
2024-02-07 14:58:54 +0700

    fix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['images.unsplash.com', 'utfs.io', 'sesna.id', 'img.youtube.com']
+        domains: ['images.unsplash.com', 'utfs.io', 'sesna.id', 'img.youtube.com', 'www.sesna.id']
     },
     experimental: {
         serverActions: {
