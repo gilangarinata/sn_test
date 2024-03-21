8a9bf3f136f3ce625d19f58963bc3bd7e10acabf
gilangarinata <gilangarinata@gmail.com>
2024-03-21 13:57:39 +0700

    fix whatsapp link

--- middleware.ts
+++ middleware.ts
@@ -55,5 +55,5 @@ export function middleware(request: NextRequest) {
 
 export const config = {
     // Matcher ignoring `/_next/` and `/api/`
-    matcher: ['/((?!api|_next/static|_next/image|favicon.ico|images).*)']
+    matcher: ['/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|images).*)']
 }
