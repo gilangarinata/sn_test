d6d553ec1e30e82aeda07d4efbf06d4db4f684de
gilangarinata <gilangarinata@gmail.com>
2025-07-24 12:28:46 +0700

    fix editor

--- components/admin/media/news/new_editor.tsx
+++ components/admin/media/news/new_editor.tsx
@@ -2,51 +2,17 @@
 
 
 import {
-    BlockNoteView, lightDefaultTheme, Theme,
     useCreateBlockNote
 } from "@blocknote/react";
+
+import {BlockNoteView, lightDefaultTheme, Theme} from "@blocknote/mantine";
 
 import "@blocknote/core/style.css";
 import {cn} from "@/lib/utils";
 import "@/app/[lang]/custom.css";
 import "@blocknote/mantine/style.css";
-import {BlockNoteEditor, PartialBlock} from "@blocknote/core";
+import {BlockNoteEditor, defaultBlockSpecs, PartialBlock} from "@blocknote/core";
 
-
-const lightRedTheme = {
-    colors: {
-        editor: {
-            text: "#222222",
-            background: "#ffffff",
-        },
-        menu: {
-            text: "#ffffff",
-            background: "#9b0000",
-        },
-        tooltip: {
-            text: "#ffffff",
-            background: "#b00000",
-        },
-        hovered: {
-            text: "#ffffff",
-            background: "#b00000",
-        },
-        selected: {
-            text: "#ffffff",
-            background: "#c50000",
-        },
-        disabled: {
-            text: "#9b0000",
-            background: "#7d0000",
-        },
-        shadow: "#640000",
-        border: "#870000",
-        sideMenu: "#bababa",
-        // highlightColors: lightDefaultTheme.colors.highlightColors,
-    },
-    borderRadius: 4,
-    fontFamily: "Helvetica Neue, sans-serif",
-} satisfies Theme;
 
 interface Props {
     onChange : (value : string) => void;
