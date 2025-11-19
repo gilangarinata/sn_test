fe977c067654592aeecfb309875ee4fabb88d1d7
Gilang Prilian Arinata <52315606+gilangarinata@users.noreply.github.com>
2025-11-19 15:08:06 +0700

    Update vision.tsx

--- components/landing/who-we-are/vision.tsx
+++ components/landing/who-we-are/vision.tsx
@@ -90,11 +90,10 @@ export function VisionMission({ourDna, director, visionMission, lang, dictionary
                         <Image src="/images/mission.png" alt="" width={80} height={80} />
                         <div className="flex flex-col text-[#FABD24]">
                             <h1 className="font-bold text-3xl">{dictionary.mission}</h1>
-                            <p
-                              dangerouslySetInnerHTML={{
-                                __html: translateText(visionMission.mission, lang)
-                              }}
+                            <div
+                              dangerouslySetInnerHTML={{ __html: translateText(visionMission.mission, lang) }}
                             />
+
                         </div>
                     </div>
                 </div>
